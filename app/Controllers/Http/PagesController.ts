68adc2cded050bf53516478166c1d4440888e7c4
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-10-13 18:51:26 +0500

    video 14

--- app/Controllers/Http/PagesController.ts
+++ app/Controllers/Http/PagesController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PagesController {
     public home ({view} : HttpContextContract){
-        return view.render('welcome')
+        return view.render('tasks/index')
     }
     public about ({view , params}:HttpContextContract) {
         const name = params.name;
